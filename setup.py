f3616a7068155f7d8f7485792fd29a7e2828f3e6
f-fl0 <florent@seqsense.com>
2022-08-18 23:37:42 +0900

    Remove unnecessary import and format

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
-from setuptools import setup, find_packages
-from codecs import open
+from setuptools import setup
 from os import path
 
 here = path.abspath(path.dirname(__file__))
@@ -7,23 +6,26 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-setup(name='pyblur',
-      version = '0.2.3',
-      description = 'Image blurring routines',
-      long_description = long_description,
-      keywords = 'blur',
-      classifiers=[
-          'Development Status :: 3 - Alpha',
-          'License :: OSI Approved :: MIT License',
-          'Intended Audience :: Developers',
-          'Intended Audience :: Science/Research',
-          'Programming Language :: Python :: 3.8',
-          'Topic :: Multimedia :: Graphics'],
-      url='http://github.com/lospooky/pyblur',
-      author='lospooky',
-      author_email='my.accounts@gmx.se',
-      license='MIT',
-      packages=['pyblur'],
-      include_package_data=True,
-      install_requires = ['numpy', 'pillow', 'scikit-image', 'scipy'],
-      zip_safe = False)
+setup(
+    name='pyblur',
+    version = '0.2.3',
+    description = 'Image blurring routines',
+    long_description = long_description,
+    keywords = 'blur',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Multimedia :: Graphics'
+    ],
+    url='http://github.com/lospooky/pyblur',
+    author='lospooky',
+    author_email='my.accounts@gmx.se',
+    license='MIT',
+    packages=['pyblur'],
+    include_package_data=True,
+    install_requires = ['numpy', 'pillow', 'scikit-image', 'scipy'],
+    zip_safe = False
+)
